cb4e70706f2115442ddd9bf10ed3566d3d60f85f
C0de5mith <34120242+C0de5mith@users.noreply.github.com>
2020-09-30 22:01:54 -0500

    Clean repository

--- src/main/java/com/github/c0de5mith/crazysudoku/rules/Rule.java
+++
@@ -1,6 +0,0 @@
-package com.github.c0de5mith.crazysudoku.rules;
-
-public interface Rule {
-    boolean checkBoard(short[][] board);
-    boolean isValid(short[][] board, int i, int j, short value);
-}
--- src/main/java/com/github/c0de5mith/crazysudoku/solver/SolverCallback.java
+++
@@ -1,5 +0,0 @@
-package com.github.c0de5mith.crazysudoku.solver;
-
-public interface SolverCallback {
-    public void call(short[][] solutions);
-}
